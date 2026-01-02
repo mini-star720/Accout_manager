b4133c550bfab4e2ba187f9d5cac2d8029ab18fa
mini-star720 <1169928768@qq.com>
2026-01-02 12:30:20 +0800

    build

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_untitled_FILE /Users/qianzr/Documents/software_engineering/实验三/untitled/build/untitled.app/Contents/MacOS/untitled)
+set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
